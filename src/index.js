af5ee5233106568883909abb65dd006225c13409
Riandika Lumaris <riandika.lumaris@outlook.com>
2021-05-12 01:27:07 +0700

    Remove react-helmet

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Helmet from 'react-helmet';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>
-    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
